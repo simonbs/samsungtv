088c8c7d4fc5db0144afabd595acc6ee557c8aef
Simon Støvring <mail@simonbs.dk>
2017-10-26 21:55:47 +0200

    Adds functions for controlling volume

--- index.js
+++ index.js
@@ -12,19 +12,10 @@ function SamsungTV(macAddress, ipAddress, port, appName) {
 
 module.exports = SamsungTV;
 
-SamsungTV.prototype.getServices = function() {
-  var switchService = new Service.Switch(this.name);
-  switchService
-    .getCharacteristic(Characteristic.On)
-    .on("set", this.setPowerState.bind(this))
-    .on("get", this.isOn.bind(this));
-  return [ switchService ]
-}
-
 SamsungTV.prototype.sendKey = function(key, callback) {
-  var url = "http://" + this.ipAddress + ":8001/api/v2/channels/samsung.remote.control?name=" + this.encodedAppName;
-  var ws = new WebSocket(url, callback);
-  ws.on("error", callback);
+  var url = "http://" + this.ipAddress + ":8001/api/v2/channels/samsung.remote.control?name=" + this.encodedAppName
+  var ws = new WebSocket(url, callback)
+  ws.on("error", callback)
   ws.on("message", function(data, flags) {
     var cmd = {
       "method": "ms.remote.control",
@@ -35,11 +26,11 @@ SamsungTV.prototype.sendKey = function(key, callback) {
         "TypeOfRemote": "SendRemoteKey"
       }
     };
-    data = JSON.parse(data);
+    data = JSON.parse(data)
     if (data.event == "ms.channel.connect") {
-      ws.send(JSON.stringify(cmd), callback);
+      ws.send(JSON.stringify(cmd), callback)
     }
-  });
+  })
 }
 
 SamsungTV.prototype.waitForTVOn = function(attemptsLeft, callback) {
@@ -47,34 +38,65 @@ SamsungTV.prototype.waitForTVOn = function(attemptsLeft, callback) {
   wol.wake(this.macAddress, function(err) {
     accessory.isOn(function(err, isOn) {
       if (isOn) {
-        callback();
+        callback()
       } else if (attemptsLeft > 0) {
         setTimeout(function() {
-          accessory.waitForTVOn(attemptsLeft - 1, callback);
-        }, 2000);
+          accessory.waitForTVOn(attemptsLeft - 1, callback)
+        }, 2000)
       } else {
-        callback("Unable to turn on Samsung TV.");
+        callback("Unable to turn on Samsung TV.")
       }
-    });
-  });
+    })
+  })
 }
 
 SamsungTV.prototype.turnOn = function(callback) {
-  this.waitForTVOn(3, callback);
+  this.waitForTVOn(3, callback)
 }
 
 SamsungTV.prototype.turnOff = function(callback) {
-  this.sendKey("KEY_POWER", callback);
+  this.sendKey("KEY_POWER", callback)
 }
 
 SamsungTV.prototype.isOn = function(callback) {
-  var url = "http://" + this.ipAddress + ":" + this.port + "/api/v2/";
+  var url = "http://" + this.ipAddress + ":" + this.port + "/api/v2/"
   return request.get(url, { timeout: 5000 }, function(err, httpResponse, body) {
     if (err || httpResponse.statusCode != 200) {
-      callback(null, false);
+      callback(null, false)
     } else {
-      callback(null, true);
+      callback(null, true)
     }
-  });
+  })
+}
+
+SamsungTV.prototype.volumeUp = function(callback) {
+  this.sendKey("KEY_VOLUP", callback)
+}
+
+SamsungTV.prototype.volumeDown = function(callback) {
+  this.sendKey("KEY_VOLDOWN", callback)
+}
+
+SamsungTV.prototype.changeVolume = function(volumeChange, callback) {
+  if (volumeChange != 0) {
+    var samsungtv = this
+    var key = volumeChange > 0 ? "KEY_VOLUP" : "KEY_VOLDOWN"
+    this.sendKey(key, function(error) {
+      if (error) {
+        return callback(error)
+      } else {
+        setTimeout(function() {
+          var next = volumeChange > 0 ? volumeChange-1 : volumeChange+1
+          samsungtv.changeVolume(next, callback)
+        }, 1000)
+      }
+    })
+  } else {
+    callback()
+  }
+}
+
+SamsungTV.prototype.toggleMute = function(callback) {
+  this.sendKey("KEY_MUTE", callback)
 }
 
